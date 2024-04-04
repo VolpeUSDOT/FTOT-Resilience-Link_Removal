005bae48853d1d024e36e24554af8f389a40e6db
kirbyledvina <Kirby.Ledvina@dot.gov>
2024-04-04 18:10:12 -0400

    Updates HTML report

--- link_removal/compile_report.R
+++ link_removal/compile_report.R
@@ -9,8 +9,12 @@ library(rmarkdown)
 args <- commandArgs(trailingOnly = TRUE)
 
 base_scen <- args[1]
+background_flows <- args[2]
+do_volume <- args[3]
 
 render('Disruption_Results.Rmd',
        params = list(
-         base_scen = base_scen)
+         base_scen = base_scen,
+         background_flows = background_flows,
+         do_volume = do_volume)
        )
